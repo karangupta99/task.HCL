302adbb62427e68bd7bffd44192a27dad2b03460
Karan Raj Gupta <150457443+karangupta99@users.noreply.github.com>
2026-01-15 00:06:47 +0530

    Update Task2.java

--- Assignment4/src/Assignment4/Task/Task2.java
+++ Assignment4/src/Assignment4/Task/Task2.java
@@ -1,5 +1,44 @@
 package Assignment4.Task;
 
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Task2 {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Connection conn=null;
+		ResultSet rs=null;
+		Statement st=null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/hcl","root","Karan@123");
+		st=conn.createStatement();
+		rs=st.executeQuery("Select * from Student");
+		while(rs.next()) {
+			System.out.println(rs.getString("stu_name"));
+			System.out.println(rs.getInt("roll_no"));
+			System.out.println(rs.getString("branch"));
+		}
+		String query="Insert into Student values('Aniket',32,'DS')";
+		int n=st.executeUpdate(query);
+		rs=st.executeQuery("Select * from Student");
+
+		System.out.println("after deletion");
+		
+		int affect=st.executeUpdate("Update Student set stu_name='Raj' where roll_no= 1");
+		st.executeUpdate("Delete from Student where roll_no = 32 ");
+		rs=st.executeQuery("Select * from Student");
+		while(rs.next()) {
+			System.out.println(rs.getString("stu_name"));
+			System.out.println(rs.getInt("roll_no"));
+			System.out.println(rs.getString("branch"));
+		}
+     
+	}
 
 }
